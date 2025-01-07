685736706e3a96484c5c029201b18aee12c1e2c1
ZubairrRiaz <160521830+ZubairrRiaz@users.noreply.github.com>
2025-01-08 01:29:43 +0500

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,6 @@
 import HeroSection from "./components/Hero";
 import Products from "./components/Products";
 
-const product = {
-  name: "Sample Product",
-  discountPrice: 20.00,
-  originalPrice: 30.00,
-  image: "/s8.png",
-  description: "This is a great product that you will love!",
-  reviews: 4.5 // Changed from "4.5 stars" to 4.5
-};
-
 
 export default function Home() {
   return (
